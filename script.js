3fbcabaea12a12c2c465392f3a351ef6e3cba878
j0sephl <jlindsay0709@gmail.com>
2025-06-01 12:54:14 -0600

    Update script.js

--- script.js
+++ script.js
@@ -554,8 +554,30 @@
             
             if (!part || part.quantity === 0) {
                 missingParts++;
+                statusIcon = `
+                    <span class="status-icon status-error">
+                        <svg viewBox="0 0 24 24">
+                            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"/>
+                        </svg>
+                    </span>
+                `;
             } else if (part.quantity < required) {
                 lowStockParts++;
+                statusIcon = `
+                    <span class="status-icon status-warning">
+                        <svg viewBox="0 0 24 24">
+                            <path d="M1 21h22L12 2 1 21zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z"/>
+                        </svg>
+                    </span>
+                `;
+            } else {
+                statusIcon = `
+                    <span class="status-icon status-success">
+                        <svg viewBox="0 0 24 24">
+                            <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"/>
+                        </svg>
+                    </span>
+                `;
             }
             
             const partElement = document.createElement('div');
@@ -569,7 +591,7 @@
                     </div>
                 </div>
                 <div class="project-actions">
-                    <span class="project-status-icon">${status}</span>
+                    ${statusIcon}
                     <button onclick="removeProjectTag('${id}', '${projectId}')" class="project-remove-btn">Remove</button>
                 </div>
             `;

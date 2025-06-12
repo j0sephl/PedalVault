98844c1b81ed647bf89a67eff97169e0380f9c79
j0sephl <jlindsay0709@gmail.com>
2025-06-12 10:03:31 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'pedalvault-v1.4';
-const STATIC_CACHE = 'pedalvault-static-v1.4';
+const CACHE_NAME = 'pedalvault-v1.5';
+const STATIC_CACHE = 'pedalvault-static-v1.5';
 
 // Only cache your own app files - no external CDNs
 const STATIC_ASSETS = [
@@ -12,8 +12,7 @@ const STATIC_ASSETS = [
     './manifest.json',
     './offline.html',
     './lib/rive.min.js',
-    './icons/icon-180x180.png',
-    './icons/icon-512x512.png',
+    '/icons/icon-180x180.png',
     './favicon.ico'
 ];
 
